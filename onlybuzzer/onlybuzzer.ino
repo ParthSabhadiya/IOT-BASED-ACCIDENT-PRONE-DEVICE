63e4f3b52c16efe0bf1f524a6028de74444b0c8e
SABHADIYA PARTH <48382717+ParthSabhadiya@users.noreply.github.com>
2019-04-18 09:48:50 +0530

    Buzzer Code

---
+++ onlybuzzer/onlybuzzer.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(6, INPUT_PULLUP); // enabling internal pull-up resistor
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  int val = digitalRead(2);
+  Serial.println(val);
+
+  if (val == LOW) {
+    digitalWrite(5, HIGH);
+  } else {
+    digitalWrite(5, LOW);
+  }
+}
